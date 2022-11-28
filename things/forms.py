b42b7035586611dbc0f56c320ae22235f1dba6a5
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2022-11-28 16:59:25 +0000

    tried to finished q1 and q2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms
+
+
 
 # Create your forms here.
+class ThingForm(forms.Form):
+    name = forms.CharField(label = 'name')
+    description = forms.Textarea(label = 'description')
+    quantity = forms.IntegerField(label = 'quantity')
